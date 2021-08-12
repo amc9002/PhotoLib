68cb06b685dc07f4b02da79d55df4c42e30dcb77
Andrej Cieraskou <9002amc@gmail.com>
2021-08-12 23:40:09 +0300

    template.js перароблены на кляс

--- TestTask/wwwroot/js/app.js
+++ TestTask/wwwroot/js/app.js
@@ -4,7 +4,7 @@ class App {
     constructor() {
         this.storage = new Store();
         this.model = new Model(this.storage);
-        this.template = new app.Template();
+        this.template = new Template();
         this.view = new app.View(this.template);
         this.controller = new app.Controller(this.model, this.view);
     }
